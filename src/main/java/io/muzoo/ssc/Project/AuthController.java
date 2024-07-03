b670fe1af6c9f273080adf1e0becff81f2cf079c
LKJTC <krit.jau@student.mahidol.edu>
2024-07-03 23:21:09 +0700

    dto record

--- src/main/java/io/muzoo/ssc/Project/AuthController.java
+++ src/main/java/io/muzoo/ssc/Project/AuthController.java
@@ -1,5 +1,6 @@
 package io.muzoo.ssc.Project;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,8 +17,32 @@ public class AuthController {
         return "Hello!";
     }
 
+    record RegisterRequest(@JsonProperty("first_name") String firstName,
+                           @JsonProperty("last_name") String lastName,
+                           String email,
+                           String password,
+                           @JsonProperty("password_confirm") String passwordConfirm
+    ){}
+
+    record RegisterResponse(Long id, @JsonProperty("first_name") String firstName,
+                            @JsonProperty("last_name") String lastName,
+                            String email){}
+
     @PostMapping(value = "/register")
-    public User register(@RequestBody User user) {
-        return userRepo.save(user);
+    public RegisterResponse register(@RequestBody RegisterRequest registerRequest) {
+        User saveUser = userRepo.save(
+                User.of(
+                        registerRequest.firstName(),
+                        registerRequest.lastName(),
+                        registerRequest.email(),
+                        registerRequest.password()
+                )
+        );
+        return new RegisterResponse(
+                saveUser.getId(),
+                saveUser.getFirstName(),
+                saveUser.getLastName(),
+                saveUser.getEmail()
+        );
     }
 }
