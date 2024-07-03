137b00224218ae06d36c4462f9dd4d8cb8f66c98
LKJTC <krit.jau@student.mahidol.edu>
2024-07-03 19:09:02 +0700

    Add database

---
+++ src/test/java/io/muzoo/ssc/webapp/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.muzoo.ssc.webapp.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
